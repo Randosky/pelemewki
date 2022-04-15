8b7543efe21eb0961dc789390cdd6e3f6c72e698
Randosky <103418173+Randosky@users.noreply.github.com>
2022-04-15 16:39:10 +0500

    pelemewki

---
+++ app/src/main/java/com/example/pelemewkiteamproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pelemewkiteamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button authorise, register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://pelemewki-team-project-default-rtdb.asia-southeast1.firebasedatabase.app");
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
